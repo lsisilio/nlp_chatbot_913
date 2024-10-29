89d2ddf1e1196eaf3b3a6e06564a60f43a8a0da1
Leonardo Sisilio <60850944+lsisilio@users.noreply.github.com>
2024-10-28 22:12:31 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import logging
 from typing import List
 from flask import Flask, request, jsonify
+import re
 
 app = Flask(__name__)
 
@@ -33,24 +34,11 @@ def webhook():
     logger.info(f"action: {action}")
     logger.info(f"callback_data: {callback_data}")
 
-    # Tratar diferentes ações baseadas na intent detectada
-    if action == 'defaultWelcomeIntent':
-        response = format_response(['Hi, how can I help you today?'])
-
-    elif action == 'input.welcome':
-        response = format_response(['testando resposta', 'apareceu aii?'])
-
-    elif action == 'city.action':
-        # Tratar o callback_data com mais cuidado, para lidar com None
-        if callback_data == 'São Paulo':
-            data = get_current_climate('São Paulo')
-            response = format_response([data['temperature_mean']])
-        elif callback_data == 'Berlin':
-            data = get_current_climate('Berlin')
-            response = format_response([data['temperature_mean']])
-        elif callback_data == 'Tokyo':
-            data = get_current_climate('Tokyo')
-            response = format_response([data['temperature_mean']])
+    if action == 'cep':
+        # Verificando se o CEP informado tem oito caracteres
+        if re.fullmatch(r'\d{8}', callback_data):
+            data = get_address(callback_data)
+            response = format_response([data])
         else:
             logger.warning(f'callback_data não reconhecido: {callback_data}')
             response = format_response(['Nenhuma opção válida foi selecionada.'])
@@ -60,60 +48,29 @@ def webhook():
 
     else:
         response = format_response([f'No handler for the action name {action}.'])
-
     return response
 
 
-import json
 import requests
-
-# OpenWeatherMap API
-WEATHER_API_BASE_URL    = 'http://api.openweathermap.org/data/2.5/forecast?'
-WEATHER_API_KEY         = os.getenv('weather_api_key')
-UNITS = 'metric'
-
-def get_current_climate(city):
-    url = WEATHER_API_BASE_URL + 'appid=' + WEATHER_API_KEY + '&q=' + city + '&units=' + UNITS
+import random
+# API ViaCEP
+# https://viacep.com.br/
+def get_address(cep):
+    url = f'https://viacep.com.br/ws/{cep}/json/'
     
-    response = requests.get(url).json()
-
+    response = requests.get(url)
+    response_data = response.json()
+    # print(response_data)
     if response.status_code == 200:
-        
-        data = {
-        'description': response['list'][0]['weather'][0]['description'],
-        'temperature': response['list'][0]['main']['temp']
-        }
-        #chat_response = f'In {city}, we have {description} and the temperature is {temperature} degrees.'
+        previsão_entrega = random.randint(1, 14)    
+        data = f"O documento será entregue na {response_data['logradouro']}, {response_data['localidade']} - {response_data['uf']} em até {previsão_entrega} dias."
         return data
     else:
         return f"Erro na requisição: {response.status_code}"
-    
+
 
 if __name__ == '__main__':
     # Pegar a porta da variável de ambiente ou usar 5000 como padrão
     port = int(os.environ.get('PORT', 5000))
     logger.info(f"Starting app on port {port}")
     app.run(host='0.0.0.0', port=port)
-
-
-
-
-
-# def webhook():
-#     req = request.get_json(silent=True, force=True)
-    
-#     # Processar a intent recebida
-#     intent_name = req.get('queryResult').get('intent').get('displayName')
-    
-#     # Tratamento baseado no nome da intent
-#     if intent_name == 'floresta negra':
-#         response_text = "Certo, um bolo floresta negra"
-#     elif intent_name == 'cenoura':
-#         response_text = "Certo, um bolo de cenoura"
-#     elif intent_name == 'red velvet':
-#         response_text = "Certo, um bolo red velvet"
-#     else:
-#         response_text = "Desculpe, não entendi sua escolha."
-
-#     # Montar a resposta para o Dialogflow
-#     return make_webhook_response(response_text)
